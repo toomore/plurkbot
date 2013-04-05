402fcb57e0bf6cdd0d446c81ebe167a7b5cd96f3
Toomore Chiang <toomore0929@gmail.com>
2013-04-06 01:44:48 +0800

    Add murmur 3, 4.

--- plurkbot.py
+++ plurkbot.py
@@ -5,22 +5,26 @@ import re
 import sys
 import setting
 from random import shuffle
-from random import sample
+from random import choice
+
 
 get_murmur_list = []
 
+actor = ['我','妳','她']
+v = ['吃了', '買了', '吃完', '買完', '沒有買', '沒有吃' , '忘記買', '忘記吃']
+time = ['剛剛', '昨天', '今天', '早上', '中午', '晚上', '深夜']
+n = ['早餐', '午餐', '晚餐', '宵夜', '點心', '雞排', '甜不辣']
+store = ['書店', '誠品', ' 7-11 ', '電影院', '火車站', '捷運上', '公車站', '公園', '轉運站', '摩斯漢堡']
+MRT = ['東門站', '頂溪站', '劍潭站', '景美站', '江子翠站']
+
+
 def murmur_list(f):
-    get_murmur_list.append(f.__name__)
+    get_murmur_list.append(f)
     return f
 
 @murmur_list
 def murmur():
-    i = ['我','妳','她']
-    vi = ['剛剛', '昨天', '今天', '早上', '中午', '晚上', '深夜']
-    v = ['吃了', '買了', '吃完', '買完', '沒有買', '沒有吃' , '忘記買', '忘記吃']
-    n = ['早餐', '午餐', '晚餐', '宵夜', '點心', '雞排', '甜不辣']
-
-    return join_word([i, vi, v, n])
+    return join_word([actor, time, v, n])
 
 @murmur_list
 def murmur_2():
@@ -28,6 +32,22 @@ def murmur_2():
     n = ['臉頰', '額頭', '小酒窩', '眉毛', '耳朵']
 
     return join_word([i, n])
+
+@murmur_list
+def murmur_3():
+    ''' 她今天在誠品看到了我，也看到了妳 '''
+    shuffle(actor)
+    store.extend(MRT)
+    return '{0}{1}在{2}看到了{3}，也看到了{4}'.format(actor[0], choice(time), choice(store), actor[1], actor[2])
+
+@murmur_list
+def murmur_4():
+    ''' 我收到妳寄來的照片，過了這麼多年，還記得她嗎？'''
+    gift = ['照片', '禮物', '信', '手札']
+    new_actor = actor[1:]
+    shuffle(new_actor)
+    return '收到{0}寄來的{1}，過了這麼多年，還記得{2}嗎？在這令人難以忘懷的{3} ...'.format(
+            new_actor[0], choice(gift), new_actor[1], choice(time[1:]))
 
 def join_word(argv):
     map(shuffle, argv)
@@ -59,5 +79,5 @@ def post_to_plurk(content):
         )
 
 if __name__ == '__main__':
-    murmur_word = locals().get(sample(get_murmur_list, 1)[0])()
+    murmur_word = choice(get_murmur_list)()
     post_to_plurk(murmur_word)
