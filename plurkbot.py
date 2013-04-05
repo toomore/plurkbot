29dbe998ac5fc491939880b54fb3f0a4e169ff1a
Toomore Chiang <toomore0929@gmail.com>
2013-04-05 18:43:19 +0800

    Add murmur_2.

--- plurkbot.py
+++ plurkbot.py
@@ -5,6 +5,7 @@ import re
 import sys
 import setting
 from random import shuffle
+from random import sample
 
 def murmur():
     i = ['我','妳','她']
@@ -12,11 +13,19 @@ def murmur():
     v = ['吃了', '買了', '吃完', '買完', '沒有買', '沒有吃' , '忘記買', '忘記吃']
     n = ['早餐', '午餐', '晚餐', '宵夜', '點心', '雞排', '甜不辣']
 
-    map(shuffle, [i, vi, v, n])
-    result = [i[0] for i in [i, vi, v, n]]
+    return join_word([i, vi, v, n])
+
+def murmur_2():
+    i = ['輕輕的摸了妳的']
+    n = ['臉頰', '額頭', '小酒窩', '眉毛', '耳朵']
+
+    return join_word([i, n])
+
+def join_word(argv):
+    map(shuffle, argv)
+    result = [i[0] for i in argv]
 
     return ''.join(result)
-
 
 def post_to_plurk(content):
     s = requests.Session()
@@ -42,4 +51,5 @@ def post_to_plurk(content):
         )
 
 if __name__ == '__main__':
-    post_to_plurk(murmur())
+    #post_to_plurk(murmur())
+    print sample([murmur, murmur_2], 1)[0]()
