e39604e8240ad254f86fdfdde749a9b18cc93632
Toomore Chiang <toomore0929@gmail.com>
2013-04-07 14:48:33 +0800

    Add reddit/python, utf8 problem.

--- plurkbot/Rss.py
+++ plurkbot/Rss.py
@@ -21,7 +21,7 @@ def newsyc50():
     ''' http://talkfast.org/2010/07/23/a-cure-for-hacker-news-overload/ '''
     feed = choice_feed('http://feeds.feedburner.com/newsyc50?format=xml')
     return '{0} ({1}) {2}'.format(
-            feed.get('link'), *feed.get('title').rsplit(' ', 1))
+            feed.get('link'), *feed.get('title').encode('utf-8').rsplit(' ', 1))
 
 
 @rss_list
@@ -29,18 +29,25 @@ def newsyc150():
     ''' http://talkfast.org/2010/07/23/a-cure-for-hacker-news-overload/ '''
     feed = choice_feed('http://feeds.feedburner.com/newsyc150?format=xml')
     return '{0} ({1}) {2}'.format(
-            feed.get('link'), *feed.get('title').rsplit(' ', 1))
+            feed.get('link'), *feed.get('title').encode('utf-8').rsplit(' ', 1))
 
 
 @rss_list
 def pypi_latest():
     feed = choice_feed('https://pypi.python.org/pypi?%3Aaction=rss')
-    return '{0} ({1}) - PyPI Recent Updates'.format(
-            feed.get('link'), feed.get('title'))
+    return '{0} ({1}) {2} - PyPI Recent Updates'.format(
+            feed.get('link'), feed.get('title'), feed.get('summary').encode('utf-8'))
 
 
 @rss_list
 def pypi_newest():
     feed = choice_feed('https://pypi.python.org/pypi?%3Aaction=packages_rss')
-    return '{0} ({1}) - PyPI Newest Packages'.format(
+    return '{0} ({1}) {2} - PyPI Newest Packages'.format(
+            feed.get('link'), feed.get('title'), feed.get('summary').encode('utf-8'))
+
+
+@rss_list
+def reddit_python():
+    feed = choice_feed('http://www.reddit.com/r/Python/.rss')
+    return '{0} ({1}) - Reddit / Python'.format(
             feed.get('link'), feed.get('title'))
